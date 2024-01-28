d77c56279d10a4f8fcb8538627a6d46412210f5c
Diego Crepaldi Marcelino <dmarcelino1989@gmail.com>
2024-01-28 15:58:06 -0300

    :sparkles: feat: Acrescentando a parte de autenticação com login e senha

--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -1,12 +1,24 @@
-import { UsButton } from "../../components/ui/button"
-import { InputEmail,InputPassword} from "../../components/ui/input"
-import imgLogin from '../../assets/img_login.png'
-import styles from './styles.module.scss'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import { UsButton } from "../../components/ui/button";
+import { InputEmail, InputPassword } from "../../components/ui/input";
+import imgLogin from "../../assets/img_login.png";
+import styles from "./styles.module.scss";
+
+import { useAuth } from "../../contexts/auth";
 
 // import GoogleLogin from 'react-google-login'
 
-
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { authLogin } = useAuth();
+
+  function handleAuthLogin() {
+    authLogin({ email, password });
+  }
 
   // const [name, setName] = useState()
   // const [email, setEmail] = useState()
@@ -17,32 +29,31 @@ export default function Login() {
   // }
 
   return (
-
-    <div className={styles.main}> 
+    <div className={styles.main}>
       <div>
-        <img src={imgLogin} alt='img Login' className={styles.imgLogin}/>
+        <img src={imgLogin} alt="img Login" className={styles.imgLogin} />
       </div>
-       
+
       <div className={styles.fom}>
         <div className={styles.formLogin}>
           <p className={styles.h1}>Entre no Orange Portfólio</p>
           <div>
             <button className={styles.button}>google</button>
           </div>
-          
+
           <form>
-          <p className={styles.p}>Faça login com email</p>
-              <InputEmail children={"Email"}/>
-              <InputPassword/>
-              <UsButton children={"Entrar"}/>
-              <a href="">Cadastre-se</a>
+            <p className={styles.p}>Faça login com email</p>
+            <InputEmail
+              children={"Email"}
+              onChance={(e) => setEmail(e.target.value)}
+            />
+            <InputPassword onChance={(e) => setPassword(e.target.value)} />
+            <UsButton children={"Entrar"} onClick={handleAuthLogin} />
+
+            <Link to="/register">Cadastre-se</Link>
           </form>
-          
         </div>
       </div>
-       
-
     </div>
-
-  )
+  );
 }
