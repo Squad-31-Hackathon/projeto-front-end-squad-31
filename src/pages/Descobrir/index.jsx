1c61a86288e3aed6b9b763dc1d97aeca17746db6
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2024-01-29 14:25:38 -0300

    Feat: Consumo de API para a página de descobrir

--- src/pages/Descobrir/index.jsx
+++ src/pages/Descobrir/index.jsx
@@ -1,5 +1,18 @@
+import { api } from "../../services/api"
+import { useState, useEffect } from "react"
+
 export function Descobrir() {
 
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    api
+      .get("/project")
+      .then((response) => setUser(response.data))
+      .catch((err) => {
+        console.error("deu erro" + err);
+      });
+  }, []);
   return (
     <div>Descobrir</div>
   )
