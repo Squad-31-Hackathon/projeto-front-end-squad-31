95c4724e2b1e920b1771f3762a7b56ec2212aedf
Diego Crepaldi Marcelino <dmarcelino1989@gmail.com>
2024-01-30 22:55:37 -0300

    :sparkles: feat: criado função para deslogar da aplicação

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -6,33 +6,35 @@ const AuthContext = createContext({});
 function AuthProvider({ children }) {
   const [data, setData] = useState({});
 
-  async function authLogin({email, password}) {
+  async function authLogin({ email, password }) {
     try {
       const response = await api.post("/auth/login", { email, password });
-      const {user, token } = response.data;
+      const { token } = response.data;
 
-      //localStorage.setItem("user", JSON.stringify(user))
-      localStorage.setItem("token", token)
+      localStorage.setItem("token", token);
 
       api.defaults.headers.authorization = `Bearer ${token}`;
-      setData({ user, token });
+      setData({ token });
 
       console.log(response);
     } catch (error) {
       if (error.response) {
-        console.log("Não foi possivel entrar",error.response.data.message);
+        console.log("Não foi possivel entrar", error.response.data.message);
       } else {
         console.log("Não foi possivel entrar");
       }
     }
   }
 
-  useEffect(() =>{
+  function signOut(){
+    localStorage.removeItem("token")
+    setData({})
+  }
 
-  }, [])
+  useEffect(() => {}, []);
 
   return (
-    <AuthContext.Provider value={{ authLogin, user: data.token }}>
+    <AuthContext.Provider value={{ authLogin, signOut,  user: data.token }}>
       {children}
     </AuthContext.Provider>
   );
