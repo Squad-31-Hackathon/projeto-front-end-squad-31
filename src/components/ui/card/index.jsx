785445a15db04ee88e4daca08eb5e12a5fd64663
Diego Crepaldi Marcelino <dmarcelino1989@gmail.com>
2024-02-03 10:48:57 -0300

    :sparkles: feat: Criando a MAP no componente

--- src/components/ui/card/index.jsx
+++ src/components/ui/card/index.jsx
@@ -1,104 +1,122 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
-import { Avatar, CardActionArea } from '@mui/material';
-import Modal from '@mui/material/Modal';
-import styles from './styles.module.scss'
-import CloseIcon from '@mui/icons-material/Close';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import { Avatar, CardActionArea } from "@mui/material";
+import Modal from "@mui/material/Modal";
+import styles from "./styles.module.scss";
+import CloseIcon from "@mui/icons-material/Close";
 
+import { api } from "../../../services/api";
+import { useState, useEffect } from "react";
 
 export default function DescobrirCard() {
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("/project")
+      .then((response) => setUser(response.data))
+      .catch((err) => {
+        console.error("deu erro" + err);
+      });
+  }, []);
 
   return (
-            <div>
-                <button className={styles.button} onClick={handleOpen}>
-                    <Card className={styles.cart}>
-                    <CardActionArea  >
-                        <CardContent>
-                            <div className={styles.card}>
-                                <img className={styles.img} src="../../../../public/semImagem.png"></img> 
-                            </div>
-                            <div className={styles.perfil}>
-                                <Avatar className={styles.perImg}/>
-                                <p>Nome Sobrenome</p>
-                                <FiberManualRecordIcon className={styles.ponto}/>
-                                <p>02/24</p>
-                            </div>
-                        </CardContent>
-                    </CardActionArea>
-                    </Card>
+    <div>
+      {user.map((dados) => (
+        <div key={dados.uuid}>
+          <button className={styles.button} onClick={handleOpen}>
+            <Card className={styles.cart}>
+              <CardActionArea>
+                <CardContent>
+                  <div className={styles.card}>
+                    <img className={styles.img} src={dados.image}></img>
+                  </div>
+                  <div className={styles.perfil}>
+                    <Avatar className={styles.perImg} />
+                    <p>
+                      {dados.owner.name} {dados.owner.lastName}
+                    </p>
+                    <FiberManualRecordIcon className={styles.ponto} />
+                    <p>02/24</p>
+                  </div>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <div className={styles.div}>
+              <div className={styles.close}>
+                <button onClick={handleClose}>
+                  <CloseIcon />
                 </button>
-                <Modal
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby="modal-modal-title"
-                    aria-describedby="modal-modal-description"
-                >
-                    <div  className={styles.div}> 
-                        <div className={styles.close}>
-                            <button onClick={handleClose}><CloseIcon/></button>
-                        </div>
-                        <div className={styles.info}>
-                            <div className={styles.perfil}>
-                                <div>
-                                    <Avatar/>
-                                </div>
-                                <div>
-                                    <p>Nome Sobrenome</p>
-                                    <p>02/24</p>
-                                </div>
-                            </div>
-                            <div className={styles.title}>
-                                <p>Titulo do Projeto</p>      
-                            </div>
-                            <div className={styles.tags}>
-                                <span>Tag1</span>
-                                <span>Tag2</span>
-                            </div>
-                        </div>
-                        <div className={styles.midle} >
-                          
-                            <img className={styles.img} src='../../../../public/semImagem.png'></img>
-                            <div className={styles.resp}>
-                                <div className={styles.perfil}>
-                                    <div>
-                                        <Avatar className={styles.avatar} />
-                                    </div>
-                                    <div className={styles.text}>
-                                        <p>Nome Sobrenome</p>
-                                        <FiberManualRecordIcon className={styles.ponto}/>
-                                        <p>02/24</p>
-                                    </div>
-                                    
-                                </div>
-                                <div className={styles.tags}>
-                                        <span>Tag1</span>
-                                        <span>Tag2</span>
-                                    </div>
-                            </div>  
-
-                        </div>
-                        <div className={styles.final}>
-                            <div className={styles.descr}>
-                                <p>  Temos o prazer de compartilhar com vocês uma variação do nosso primeiro recurso gratuito.
-                                     É um modelo de IA.
-                                     Tentamos redesenhar uma versão mais minimalista do nosso primeiro projeto.</p>
-                            </div>
-                            <div className={styles.link}>
-                                <p>Download</p>
-                                <a href="#">https://gumroad.com/products/wxCSL</a>
-                            </div>
-                        </div>
+              </div>
+              <div className={styles.info}>
+                <div className={styles.perfil}>
+                  <div>
+                    <Avatar />
+                  </div>
+                  <div>
+                    <p>
+                      {dados.owner.name} {dados.owner.lastName}
+                    </p>
+                    <p>02/24</p>
+                  </div>
+                </div>
+                <div className={styles.title}>
+                  <p>{dados.title}</p>
+                </div>
+                <div className={styles.tags}>
+                  <span>Tag1</span>
+                  <span>Tag2</span>
+                </div>
+              </div>
+              <div className={styles.midle}>
+                <img className={styles.img} src={dados.image}></img>
+                <div className={styles.resp}>
+                  <div className={styles.perfil}>
+                    <div>
+                      <Avatar className={styles.avatar} />
                     </div>
-                    
-                </Modal>
-                
-
+                    <div className={styles.text}>
+                      <p>
+                        {dados.owner.name} {dados.owner.lastName}
+                      </p>
+                      <FiberManualRecordIcon className={styles.ponto} />
+                      <p>02/24</p>
+                    </div>
+                  </div>
+                  <div className={styles.tags}>
+                    <span>Tag1</span>
+                    <span>Tag2</span>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.final}>
+                <div className={styles.descr}>
+                  <p>
+                    {dados.description}
+                  </p>
+                </div>
+                <div className={styles.link}>
+                  <p>Download</p>
+                  <a href="#">{dados.link}</a>
+                </div>
+              </div>
             </div>
+          </Modal>
+        </div>
+      ))}
+    </div>
   );
 }
